237d55c1e8c10deb07e6adeec5d029afe41a1d57
19endok10 <19endok10@millshighsch.org>
2017-08-22 09:29:05 -0700

    Kaitlyn endo

---
+++ Project2/src/Project2.java
@@ -0,0 +1,13 @@
+//Kaitlyn Endo
+//project description: my second project
+//Date: August 22, 2017
+
+
+public class Project2 {
+
+	public static void main(String[] args) {
+		System.out.println("hello, this is proooject 2!!");
+
+	}
+
+}
