2d15f8d0ff2b9972369caaef7387edeafec407ff
19endok10 <19endok10@millshighsch.org>
2017-08-29 11:41:00 -0700

    this is more third try of pokemon

---
+++ Project2/src/Pokemon.java
@@ -0,0 +1,14 @@
+//Kaitlyn Endo
+//project description: making faces
+//Date: August 28, 2017
+
+
+public class Pokemon{
+public static void main (String[]args) {
+	System.out.println("\t\t\tPikachu welcomes you to the world of pokemon!");
+	System.out.println("\t\t\t\t\t (\\__/)");
+	System.out.println("\t\t\t\t\t (o^.^)");
+	System.out.println("\t\t\t\t\tz(_(\")(\")");
+	System.out.println();
+	}
+}
