0735a331fb3385f59f6baf999404894b6393f4d6
19endok10 <19endok10@millshighsch.org>
2017-09-05 09:27:34 -0700

    my calculator mthod

---
+++ Calculate Library/Calculate.java
@@ -0,0 +1,12 @@
+/*Kaitlyn Endo 
+September 5, 2017
+this is our home ground library that will contain various mathematical method 
+*/
+public class Calculate {
+
+	public static int square (int operand) {
+		return operand * operand;
+/*This method takes an integer and return its square*/
+	}
+}
+
