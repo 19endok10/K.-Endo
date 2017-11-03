6e0979f7e914a923fa6a2f25e6edbe2e41797026
19endok10 <19endok10@millshighsch.org>
2017-11-03 08:55:32 -0700

    hello

---
+++ array mini lab/src/ArrayLab3.java
@@ -0,0 +1,8 @@
+/* Kaitlyn Endo 
+ * 10/31
+ */
+import java.util.*; 
+public class ArrayLab3 {
+	public static void main (String[] args) {
+	}
+
---
+++ array mini lab/src/LotsOfCopies.java
@@ -0,0 +1,36 @@
+/* Kaitlyn Endo 
+ * 11/2/17
+ */
+import java.util.*;
+public class LotsOfCopies{
+	public static void main (String[] args) {
+		int num = 7;
+		String strMain = "APCS";
+		int [] arrMain = {1, 2, 3, 4, 5};
+		changeMe(num, strMain, arrMain);
+		System.out.println(num);
+		System.out.println(strMain);
+		System.out.println(Arrays.toString(arrMain));
+	}
+	
+	
+	public static void changeMe (int x, String str, int[] arr) {
+		x=8
+		str = "Hello";
+		for (int i = 0; i < arr.length; i++) {
+		arr[i]++;
+		}
+		// a = startValue
+		// b = a
+		// a = newValue 
+		// x = 7; x is a first a
+		int b = x;
+		x = 3;
+		System.out.println("bstr:"+ btsr);
+		
+		int[] barr =arr;
+		for [int i = 0, ]
+	
+	
+}	
+
