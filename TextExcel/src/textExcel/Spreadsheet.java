a0c2338608430e931713486acef2cc84b218d1c8
19endok10 <19endok10@millshighsch.org>
2018-03-13 09:15:36 -0700

    hi

---
+++ TextExcel/src/textExcel/Spreadsheet.java
@@ -0,0 +1,45 @@
+package textExcel;
+
+// Update this file with your own code.
+
+public class Spreadsheet implements Grid{
+	private int numOfrows;
+	private int numOfcols;
+	private int emptycell;
+	
+	@Override
+	public String processCommand(String command)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getRows()
+	{
+		// TODO Auto-generated method stub
+		return
+	}
+
+	@Override
+	public int getCols()
+	{
+		// TODO Auto-generated method stub
+		return
+	}
+
+	@Override
+	public Cell getCell(Location loc)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getGridText()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
